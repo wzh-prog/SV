a87d9c6a0d952862ce53e4dc60a8e988e1861fc9
Chen Chen <chenchen@local>
2023-10-18 10:18:14 +0800

    1018

--- video_refs/train_t.py
+++ video_refs/train_t.py
@@ -255,7 +255,7 @@ def main(args):
     )
 
     print("Creating model")
-    model = torchvision.models.get_model(args.model, weights=args.weights)
+    model = torchvision.models.get_model("r2plus1d_18", weights="KINETICS400_V1")
     model.to(device)
     if args.distributed and args.sync_bn:
         model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
